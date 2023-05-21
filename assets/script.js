94b2665432395a228d366c18dd0c4fcd7dfbc086
Jeremy <jectaunscripted@gmail.com>
2023-05-21 20:33:19 +0200

    add preloader + faster loading times
    faster loading times have been achieved by making assets local. Also i have sorted local files into a folder to keep the repository organized.

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function() {
+    const preloader = document.getElementById('preloader');
+    setTimeout(function() {
+      preloader.classList.add('fade-out');
+      setTimeout(function() {
+        preloader.style.display = 'none';
+      }, 1000); // Fade-out duration (in milliseconds)
+    }, 2500); // Adjust the delay time as needed (in milliseconds)
+  });
